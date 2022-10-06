c0e2bab51179bccb9adcdfca784aaae10b13d225
Bruno Costa e Silva <bruno.silva@squadra.com.br>
2022-10-06 09:10:20 -0300

    Implementação inicial Formação Threads Java

---
+++ threads/src/br/com/bruno/threads/Principal.java
@@ -0,0 +1,9 @@
+package br.com.bruno.threads;
+
+public class Principal {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Isso é o thread main");
+        Thread.sleep(30000);
+
+    }
+}
